ba491be73539551d5191c78b95a90c10e4972fff
David Grierson <david.grierson@sky.uk>
2016-10-06 18:32:43 +0100

    Got connections and message listeners working.

--- homme-bot/src/main/java/uk/org/grierson/bots/HommeBot.java
+++ homme-bot/src/main/java/uk/org/grierson/bots/HommeBot.java
@@ -7,16 +7,53 @@ import org.apache.logging.log4j.Logger;
 
 import com.bskyb.cbs.sdlc.bots.BotException;
 import com.bskyb.cbs.sdlc.bots.SingleChannelBotService;
+import com.ullink.slack.simpleslackapi.events.SlackMessagePosted;
 
 public class HommeBot extends SingleChannelBotService {
     private static final String HOMME_PROPERTIES_FILE = "src/main/resources/homme.properties";
     private static final Logger LOGGER = LogManager.getLogger(HommeBot.class);
-
+    
     public HommeBot(String channel) throws BotException {
         super(channel, Paths.get(HOMME_PROPERTIES_FILE));
         
-        LOGGER.info("Started HommeBot");
+        LOGGER.info("Started HommeBot on [" + channel + "]");
+        
         LOGGER.debug("Adding listener");
+        addMessagePostedListener(new HommeMessagePostedListener(this));
     }
     
+    public boolean messageContainsTriplet(String message) {
+        return message.contains("how");
+    }
+
+    public void processDirectMessage(SlackMessagePosted event) throws BotException {
+        String message = event.getMessageContent();
+        String response = "";
+        
+        switch ( message ) {
+            case "help" :
+                response = getHelp();
+                break;
+            case "scorecard" :
+                response = getScorecard();
+                break;
+            default:
+                response = unknownCommand();
+        }
+        
+        sendDirectMessage(event.getSender(), response);
+    }
+
+    private String unknownCommand() {
+        return "I'm sorry I don't understand that command - try \"help\"";
+    }
+
+    private String getScorecard() {
+        // TODO Auto-generated method stub
+        return "This will return a scorecard";
+    }
+
+    private String getHelp() throws BotException {
+        return getProperty("help-text");
+    }
 }
---
+++ homme-bot/src/main/java/uk/org/grierson/bots/HommeMessagePostedListener.java
@@ -0,0 +1,57 @@
+package uk.org.grierson.bots;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.bskyb.cbs.sdlc.bots.*;
+import com.ullink.slack.simpleslackapi.SlackSession;
+import com.ullink.slack.simpleslackapi.SlackUser;
+import com.ullink.slack.simpleslackapi.events.SlackMessagePosted;
+
+public class HommeMessagePostedListener extends MessagePostedListener {
+    private static final Logger LOGGER = LogManager.getLogger(HommeMessagePostedListener.class);
+
+    public HommeMessagePostedListener(HommeBot bot) {
+        super(bot);
+        LOGGER.debug("Added as a listener"); 
+    }
+
+    @Override
+    public void onEvent(SlackMessagePosted event, SlackSession session) {
+        LOGGER.trace("Received message posted event");
+        LOGGER.trace("Channel == [" + event.getChannel().getName() + "]");
+
+        if ( event.getChannel().isDirect() ) {
+            LOGGER.debug("Got a direct message from [" + event.getSender().getRealName() + "]");
+            
+            try {
+                ((HommeBot) bot).processDirectMessage(event);
+            }
+            catch ( BotException e ) {
+                LOGGER.error("Unable to send a message to [" + event.getSender().getRealName() + "]"); 
+            }
+            
+            return;
+        }
+            
+        // Only process events from the correct channel for this bot. 
+        if ( ! event.getChannel().getName().equals(bot.getChannel().getName()) ) {
+            return;
+        }
+        
+        // Ignore our own messages;
+        if ( event.getSender().getId().equals(session.sessionPersona().getId()) ) {
+            return;
+        }
+        
+        SlackUser sender = event.getSender();
+        String message = event.getMessageContent();
+        
+        LOGGER.trace("Sender == [" + sender.getId() + "]");
+        LOGGER.trace("Received Message == [" + message + "]");
+        
+        if ( ((HommeBot) bot).messageContainsTriplet(message) ) {
+            bot.sendMessage("[Hiccup]");
+        }
+    }
+}
